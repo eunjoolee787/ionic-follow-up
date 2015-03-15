a4266f8485b60bbdc07890211859f552b6917532
Eun Joo <eunjoolee787@gmail.com>
2015-03-15 02:25:43 -1000

    git init

---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($http) {
+  var app = this;
+  app.people = [];
+
+    app.addPerson = function (person) {
+      $http.post("http://localhost:4000/form", person)
+        .success(function (data) {
+          app.people = data;
+          console.log(data);
+        })
+        .error(function (error) {
+          console.log(error);
+        })
+    }
+
+})
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
