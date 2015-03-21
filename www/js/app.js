54d61c5080cd7d109754bfecab6287b3d5cc83ef
Eun Joo <eunjoolee787@gmail.com>
2015-03-20 16:09:39 -1000

    Revise code for cordova camera-pending

--- www/js/app.js
+++ www/js/app.js
@@ -21,6 +21,30 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 })
 
+// exampleApp.controller("ExampleController", function($scope, $cordovaCamera) {
+ 
+//     $scope.takePicture = function() {
+//         var options = { 
+//             quality : 75, 
+//             destinationType : Camera.DestinationType.DATA_URL, 
+//             sourceType : Camera.PictureSourceType.CAMERA, 
+//             allowEdit : true,
+//             encodingType: Camera.EncodingType.JPEG,
+//             targetWidth: 300,
+//             targetHeight: 300,
+//             popoverOptions: CameraPopoverOptions,
+//             saveToPhotoAlbum: false
+//         };
+ 
+//         $cordovaCamera.getPicture(options).then(function(imageData) {
+//             $scope.imgURI = "data:image/jpeg;base64," + imageData;
+//         }, function(err) {
+//             // An error occured. Show a message to the user
+//         });
+//     }
+ 
+// });
+
 module.controller('PictureCtrl', function($scope, $cordovaCamera) {
 
   document.addEventListener("deviceready", function () {
